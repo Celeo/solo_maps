aea404b6941a4df0f3a84b51474aae2ecf595ba9
Celeo <mattboulanger@fastmail.com>
2021-07-19 15:54:35 -0700

    Better image loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,51 @@ import "./App.css";
 import imageList from "./images.json";
 
 interface ImageInfo {
-  fileName: string;
   zoneName: string;
   data: any;
 }
 
-export const App = () => {
-  const [images, setImages] = React.useState<Array<ImageInfo>>([]);
+interface AppState {
+  images: Array<ImageInfo>;
+}
 
-  React.useEffect(() => {
-    const loadImages = async () => {
-      const allImages: Array<ImageInfo> = [];
-      for (const imageName of imageList) {
-        const image = await import(`../assets/${imageName}`);
-        allImages.push({
-          fileName: imageName,
-          zoneName: imageName.slice(0, -4),
-          data: image.default,
-        });
-      }
-      setImages(allImages);
-    };
-    loadImages();
-  }, []);
+export class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { images: [] };
+  }
 
-  return (
-    <div className="large-container">
-      <div>{Object.keys(images).length === 0 && <h1>Loading images</h1>}</div>
-      <div className="images-display">
-        {images.map((info) => (
-          <div key={info.zoneName}>
-            <h2>{info.zoneName}</h2>
-            <img src={info.data} alt="Map" />
-            <div className="vert-spacer"></div>
-          </div>
-        ))}
+  async componentDidMount() {
+    for (const imageName of imageList) {
+      const image = await import(`../assets/${imageName}`);
+      const info = {
+        zoneName: imageName.slice(0, -4),
+        data: image.default,
+      };
+      this.setState(({ images }) => ({
+        images: [...images, info],
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <div className="large-container">
+        <div>
+          {Object.keys(this.state.images).length === 0 && (
+            <h1>Loading images</h1>
+          )}
+        </div>
+        <div className="images-display">
+          {this.state.images.map((info) => (
+            <div key={info.zoneName}>
+              <h2>{info.zoneName}</h2>
+              <img src={info.data} alt="Map" />
+              <div className="vert-spacer"></div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
